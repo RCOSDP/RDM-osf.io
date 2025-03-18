5d5ddeb6c0f4ecedd61bfbfda8d214c0f13b9fa7
Yusaku Kitabatake <y-kitabatake@web-trickster.com>
2025-03-18 13:05:42 +0900

    addons/metadata/migration rollback

--- osf/migrations/0229_loa.py
+++ osf/migrations/0229_loa.py
@@ -20,12 +20,23 @@ class Migration(migrations.Migration):
             name='LoA',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
-                ('modified', django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified')),
+                (
+                    'created',
+                    django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created'),
+                ),
+                (
+                    'modified',
+                    django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified'),
+                ),
                 ('aal', models.IntegerField(blank=True, choices=[(1, 'AAL1'), (2, 'AAL2')], null=True)),
                 ('ial', models.IntegerField(blank=True, choices=[(1, 'IAL1'), (2, 'IAL2')], null=True)),
                 ('institution', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='osf.Institution')),
-                ('modifier', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    'modifier',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL, null=True
+                    ),
+                ),
             ],
             options={
                 'permissions': (('view_loa', 'Can view loa'), ('admin_loa', 'Can manage loa')),
