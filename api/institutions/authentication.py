845a33ee536ab27af7040b8afba398692c32e6af
Yusaku Kitabatake <y-kitabatake@web-trickster.com>
2025-03-24 23:17:25 +0900

    R-2024-AUTH01/gakuninIdentityAssuranceMethodReference

--- api/institutions/authentication.py
+++ api/institutions/authentication.py
@@ -183,6 +183,9 @@ class InstitutionAuthentication(BaseAuthentication):
         organization_name_ja = get_next(p_user, 'jao', 'jaOrganizationName')
         # affiliation: 'jaou' is friendlyName
         organizational_unit_ja = get_next(p_user, 'jaou', 'jaOrganizationalUnitName')
+        # @R-2024-AUTH01 gakuninIdentityAssuranceMethodReference
+        gakunin_identity_assurance_method_reference = p_user.get('gakuninIdentityAssuranceMethodReference')
+
         # @R2022-48 ial,aal
         ial = None
         aal = None
@@ -492,6 +495,7 @@ class InstitutionAuthentication(BaseAuthentication):
                 'organizational_unit': organizational_unit,
                 'organization_name_ja': organization_name_ja,
                 'organizational_unit_ja': organizational_unit_ja,
+                'gakunin_identity_assurance_method_reference': gakunin_identity_assurance_method_reference,
             },
         )
 
