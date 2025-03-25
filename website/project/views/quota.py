9b2992184550901524329f153bd80e26e7435b6d
ncongthang <ncongthang@tma.com.vn>
2025-03-25 15:39:33 +0700

    ref [Investigate]2.1.小型大量ファイル操作性能向上の調査・設計: Update log for s3compact storage

--- website/project/views/quota.py
+++ website/project/views/quota.py
@@ -16,10 +16,21 @@ def get_creator_quota(pid, **kwargs):
 def get_quota_from_pid(pid):
     """Auxiliary function for getting the quota from a project ID.
     Used on requests by waterbutler and the user (from browser)."""
+    import datetime
+    import time
+    import logging
+    logger = logging.getLogger(__name__)
+    begin = time.time()
+    logger.info(
+        f"--------------Begin get_quota_from_pid : {datetime.datetime.fromtimestamp(begin).strftime('%H:%M:%S.%f')[:-3]}--------------")
     node = AbstractNode.load(pid)
     max_quota, used_quota = quota.get_quota_info(
         node.creator, quota.get_project_storage_type(node)
     )
+    logger.info(
+        f"--------------End get_quota_from_pid : {datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S.%f')[:-3]}--------------")
+    logger.info(
+        f"--------------Total time get_quota_from_pid : {datetime.datetime.fromtimestamp(time.time() - begin).strftime('%H:%M:%S.%f')[:-3]}--------------")
     return {
         'max': max_quota * api_settings.SIZE_UNIT_GB,
         'used': used_quota
