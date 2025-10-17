f8a910e0ec641e1dfb30ba94903526e62d336867
Yusaku Kitabatake <y-kitabatake@web-trickster.com>
2025-10-17 15:46:36 +0900

    R-2025-23 Optimization.

--- osf/models/__init__.py
+++ osf/models/__init__.py
@@ -1,14 +1,34 @@
-from osf.models.metaschema import RegistrationSchemaBlock, RegistrationSchema, FileMetadataSchema  # noqa
+from osf.models.metaschema import (
+    RegistrationSchemaBlock,
+    RegistrationSchema,
+    FileMetadataSchema,
+)  # noqa
 from osf.models.base import Guid, BlackListGuid  # noqa
 from osf.models.user import OSFUser, Email, UserExtendedData  # noqa
-from osf.models.contributor import Contributor, RecentlyAddedContributor, PreprintContributor, DraftRegistrationContributor  # noqa
+from osf.models.contributor import (
+    Contributor,
+    RecentlyAddedContributor,
+    PreprintContributor,
+    DraftRegistrationContributor,
+)  # noqa
 from osf.models.session import Session  # noqa
 from osf.models.institution import Institution  # noqa
 from osf.models.collection import CollectionSubmission, Collection  # noqa
 from osf.models.draft_node import DraftNode  # noqa
 from osf.models.node import AbstractNode, Node  # noqa
-from osf.models.sanctions import Sanction, Embargo, Retraction, RegistrationApproval, DraftRegistrationApproval, EmbargoTerminationApproval  # noqa
-from osf.models.registrations import Registration, DraftRegistrationLog, DraftRegistration  # noqa
+from osf.models.sanctions import (
+    Sanction,
+    Embargo,
+    Retraction,
+    RegistrationApproval,
+    DraftRegistrationApproval,
+    EmbargoTerminationApproval,
+)  # noqa
+from osf.models.registrations import (
+    Registration,
+    DraftRegistrationLog,
+    DraftRegistration,
+)  # noqa
 from osf.models.nodelog import NodeLog  # noqa
 from osf.models.filelog import FileLog  # noqa
 from osf.models.preprintlog import PreprintLog  # noqa
@@ -19,23 +39,41 @@ from osf.models.citation import CitationStyle  # noqa
 from osf.models.archive import ArchiveJob, ArchiveTarget  # noqa
 from osf.models.queued_mail import QueuedMail  # noqa
 from osf.models.external import ExternalAccount, ExternalProvider  # noqa
-from osf.models.oauth import ApiOAuth2Application, ApiOAuth2PersonalToken, ApiOAuth2Scope  # noqa
+from osf.models.oauth import (
+    ApiOAuth2Application,
+    ApiOAuth2PersonalToken,
+    ApiOAuth2Scope,
+)  # noqa
 from osf.models.osf_group import OSFGroup  # noqa
 from osf.models.osf_grouplog import OSFGroupLog  # noqa
 from osf.models.licenses import NodeLicense, NodeLicenseRecord  # noqa
 from osf.models.private_link import PrivateLink  # noqa
-from osf.models.notifications import NotificationDigest, NotificationSubscription  # noqa
+from osf.models.notifications import (
+    NotificationDigest,
+    NotificationSubscription,
+)  # noqa
 from osf.models.spam import SpamStatus, SpamMixin  # noqa
 from osf.models.subject import Subject  # noqa
-from osf.models.provider import AbstractProvider, CollectionProvider, PreprintProvider, WhitelistedSHAREPreprintProvider, RegistrationProvider  # noqa
+from osf.models.provider import (
+    AbstractProvider,
+    CollectionProvider,
+    PreprintProvider,
+    WhitelistedSHAREPreprintProvider,
+    RegistrationProvider,
+)  # noqa
 from osf.models.preprint import Preprint  # noqa
 from osf.models.request import NodeRequest, PreprintRequest  # noqa
 from osf.models.identifiers import Identifier  # noqa
 from osf.models.files import (  # noqa
     BaseFileNode,
     BaseFileVersionsThrough,
-    File, Folder,  # noqa
-    FileVersion, TrashedFile, TrashedFileNode, TrashedFolder, FileVersionUserMetadata,  # noqa
+    File,
+    Folder,  # noqa
+    FileVersion,
+    TrashedFile,
+    TrashedFileNode,
+    TrashedFolder,
+    FileVersionUserMetadata,  # noqa
 )  # noqa
 from osf.models.metadata import FileMetadataRecord  # noqa
 from osf.models.node_relation import NodeRelation  # noqa
@@ -47,7 +85,11 @@ from osf.models.banner import ScheduledBanner  # noqa
 from osf.models.quickfiles import QuickFilesNode  # noqa
 from osf.models.dismissed_alerts import DismissedAlert  # noqa
 from osf.models.action import ReviewAction  # noqa
-from osf.models.action import NodeRequestAction, PreprintRequestAction, ReviewAction  # noqa
+from osf.models.action import (
+    NodeRequestAction,
+    PreprintRequestAction,
+    ReviewAction,
+)  # noqa
 from osf.models.storage import ProviderAssetFile  # noqa
 from osf.models.chronos import ChronosJournal, ChronosSubmission  # noqa
 from osf.models.blacklisted_email_domain import BlacklistedEmailDomain  # noqa
@@ -55,7 +97,9 @@ from osf.models.brand import Brand  # noqa
 from osf.models.rdm_announcement import RdmAnnouncement, RdmAnnouncementOption  # noqa
 from osf.models.rdm_addons import RdmAddonOption, RdmAddonNoInstitutionOption  # noqa
 from osf.models.rdm_statistics import RdmStatistics  # noqa
-from osf.models.rdm_file_timestamptoken_verify_result import RdmFileTimestamptokenVerifyResult  # noqa
+from osf.models.rdm_file_timestamptoken_verify_result import (
+    RdmFileTimestamptokenVerifyResult,
+)  # noqa
 from osf.models.rdm_user_key import RdmUserKey  # noqa
 from osf.models.rdm_timestamp_grant_pattern import RdmTimestampGrantPattern  # noqa
 from osf.models.timestamp_task import TimestampTask  # noqa
@@ -64,11 +108,16 @@ from osf.models.user_quota import UserQuota  # noqa
 from osf.models.project_storage_type import ProjectStorageType  # noqa
 from osf.models.region_external_account import RegionExternalAccount  # noqa
 from osf.models.institution_entitlement import InstitutionEntitlement  # noqa
+from osf.models.loa import LoA  # noqa
 from osf.models.export_data_location import ExportDataLocation  # noqa
 from osf.models.export_data import ExportData  # noqa
 from osf.models.export_data_restore import ExportDataRestore  # noqa
 from osf.models.project_limit_number_default import ProjectLimitNumberDefault  # noqa
 from osf.models.project_limit_number_template import ProjectLimitNumberTemplate  # noqa
-from osf.models.project_limit_number_template_attribute import ProjectLimitNumberTemplateAttribute  # noqa
+from osf.models.project_limit_number_template_attribute import (
+    ProjectLimitNumberTemplateAttribute,
+)  # noqa
 from osf.models.project_limit_number_setting import ProjectLimitNumberSetting  # noqa
-from osf.models.project_limit_number_setting_attribute import ProjectLimitNumberSettingAttribute  # noqa
+from osf.models.project_limit_number_setting_attribute import (
+    ProjectLimitNumberSettingAttribute,
+)  # noqa
