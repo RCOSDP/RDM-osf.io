715a982209cb8ed18fe0ea5994545645e8600bdf
Yusaku Kitabatake <y-kitabatake@web-trickster.com>
2025-11-21 16:33:49 +0900

    Redmine 56420 Test

--- admin/loa/views.py
+++ admin/loa/views.py
@@ -9,17 +9,50 @@ from django.utils.translation import ugettext_lazy as _
 from admin.rdm.utils import RdmPermissionMixin
 from admin.loa.forms import LoAForm
 from osf.models import Institution, LoA
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.http import Http404
+from admin.base.utils import render_bad_request_response
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-class ListLoA(RdmPermissionMixin, TemplateView):
+class ListLoA(RdmPermissionMixin, UserPassesTestMixin, TemplateView):
     template_name = 'loa/list.html'
     raise_exception = True
+    institution_id = None
     model = LoA
 
     form_class = LoAForm
+
+    def dispatch(self, request, *args, **kwargs):
+
+        # login check
+        if not self.is_authenticated:
+            return self.handle_no_permission()
+        try:
+            self.institution_id = self.request.GET.get('institution_id')
+            if self.institution_id:
+                self.institution_id = int(self.institution_id)
+            return super(ListLoA, self).dispatch(request, *args, **kwargs)
+        except ValueError:
+            return render_bad_request_response(request=request, error_msgs='institution_id must be a integer')
+
+    def test_func(self):
+        """check user permissions"""
+        if not self.institution_id:
+            # superuser or admin has an institution
+            return self.is_super_admin or self.is_institutional_admin
+        else:
+            # institution not exist
+            if not Institution.objects.filter(id=self.institution_id).exists():
+                raise Http404(
+                    'Institution with id "{}" not found.'.format(
+                        self.institution_id
+                    ))
+            # superuser or institutional admin has permission
+            return self.is_super_admin or \
+                (self.is_admin and self.is_affiliated_institution(self.institution_id))
 
     def get_context_data(self, **kwargs):
         user = self.request.user
@@ -46,8 +79,36 @@ class ListLoA(RdmPermissionMixin, TemplateView):
         return super(ListLoA, self).get_context_data(**kwargs)
 
 
-class BulkAddLoA(RdmPermissionMixin, View):
+class BulkAddLoA(RdmPermissionMixin, UserPassesTestMixin, View):
     raise_exception = True
+    institution_id = None
+
+    def dispatch(self, request, *args, **kwargs):
+        """Initialize attributes shared by all view methods."""
+        # login check
+        if not self.is_authenticated:
+            return self.handle_no_permission()
+        try:
+            self.institution_id = self.request.POST.get('institution_id')
+            if self.institution_id:
+                self.institution_id = int(self.institution_id)
+            else:
+                return render_bad_request_response(request=request, error_msgs='institution_id is required')
+            return super(BulkAddInstitutionEntitlement, self).dispatch(request, *args, **kwargs)
+        except ValueError:
+            return render_bad_request_response(request=request, error_msgs='institution_id must be a integer')
+
+    def test_func(self):
+        """check user permissions"""
+        # institution not exist
+        if not Institution.objects.filter(id=self.institution_id, is_deleted=False).exists():
+            raise Http404(
+                'Institution with id "{}" not found.'.format(
+                    self.institution_id
+                ))
+        # superuser or institutional admin has permission
+        return self.is_super_admin or \
+            (self.is_admin and self.is_affiliated_institution(self.institution_id))
 
     def post(self, request):
         institution_id = request.POST.get('institution_id')
