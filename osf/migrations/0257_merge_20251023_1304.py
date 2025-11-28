c35e69eb38da8c025c3753f8141ec1cdc378291d
Yusaku Kitabatake <y-kitabatake@web-trickster.com>
2025-11-28 14:53:59 +0900

    Completion for rebase.

--- osf/migrations/0257_merge_20251023_1304.py
+++ osf/migrations/0257_merge_20251023_1304.py
@@ -9,6 +9,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('osf', '0256_ensure_schema_mappings'),
         ('osf', '0254_reorder_japan_grant_number'),
+        ('osf', '0258_r_2025_23_55789'),
     ]
 
     operations = [
