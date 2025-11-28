3b93a2c8179dc7f8e9c3a05e0c0486679c078fb6
Yusaku Kitabatake <y-kitabatake@web-trickster.com>
2025-11-28 15:45:26 +0900

    Completion for rebase.

--- api/institutions/authentication.py
+++ api/institutions/authentication.py
@@ -248,7 +248,7 @@ class InstitutionAuthentication(BaseAuthentication):
                 + '&target='
                 + CAS_SERVER_URL
                 + '/login?service='
-                + urljoin(DOMAIN, "/profile/")
+                + urljoin(DOMAIN, '/profile/')
             )
             mfa_url_tmp = (
                 CAS_SERVER_URL
