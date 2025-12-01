777c878c39c44ce2c79372d81c41153fd22c6770
Yusaku Kitabatake <y-kitabatake@web-trickster.com>
2025-12-01 14:02:54 +0900

    Refactoring test for redmine 56420

--- api/institutions/authentication.py
+++ api/institutions/authentication.py
@@ -8,7 +8,7 @@ import waffle
 
 # @R2022-48 loa
 import re
-import urllib.parse
+from urllib.parse import urlencode
 
 #from django.utils import timezone
 from rest_framework.authentication import BaseAuthentication
@@ -241,20 +241,22 @@ class InstitutionAuthentication(BaseAuthentication):
         mfa_url = ''
         mfa_url_tmp = ''
         if type(p_idp) is str:
-            mfa_url_q = (
-                OSF_MFA_URL
-                + '?entityID='
-                + p_idp
-                + '&target='
-                + CAS_SERVER_URL
-                + '/login?service='
-                + urllib.parse.quote(urljoin(DOMAIN, '/profile/'), safe='')
-            )
-            mfa_url_tmp = (
-                CAS_SERVER_URL
-                + '/logout?service='
-                + urllib.parse.quote(mfa_url_q, safe='')
-            )
+            profile_url = urljoin(DOMAIN, "/profile/")
+
+            login_url = CAS_SERVER_URL + '/login?' + urlencode({
+                'service': profile_url,
+            })
+
+            mfa_url_q = OSF_MFA_URL + '?' + urlencode({
+                'entityID': p_idp,
+                'target': login_url,
+            })
+
+            # CAS logout → MFA の redirect
+            mfa_url_tmp = CAS_SERVER_URL + '/logout?' + urlencode({
+                'service': mfa_url_q
+            })
+
         loa_flag = True
         loa = LoA.objects.get_or_none(institution_id=institution.id)
         if loa:
