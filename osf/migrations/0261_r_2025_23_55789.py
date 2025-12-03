d92d5888c8abc916e3adf12cca704078dd2f37aa
Yusaku Kitabatake <y-kitabatake@web-trickster.com>
2025-12-03 14:34:53 +0900

    Resolve conflicts with the develop

--- osf/migrations/0261_r_2025_23_55789.py
+++ osf/migrations/0261_r_2025_23_55789.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('osf', '0257_merge_20251023_1304'),
+        ('osf', '0260_merge_20251126_1230'),
     ]
 
     operations = [
