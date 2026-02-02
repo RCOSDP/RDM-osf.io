d3893172cd9ebc385703ae885938092471e76e84
hide24 <hidetoshi.yoshimoto@gmail.com>
2026-02-02 09:38:42 +0900

    fix: tests

--- api_tests/institutions/views/test_institution_auth.py
+++ api_tests/institutions/views/test_institution_auth.py
@@ -12,7 +12,6 @@ from framework.auth import signals, Auth
 from framework.auth.views import send_confirm_email
 
 from osf.models import OSFUser
-from osf.models import UserExtendedData
 from osf_tests.factories import InstitutionFactory, ProjectFactory, UserFactory
 
 from tests.base import capture_signals
@@ -539,32 +538,6 @@ class TestInstitutionAuth:
         user = OSFUser.objects.filter(username='tmp_eppn_' + username).first()
         assert user
         assert user.jobs[0]['department'] == organizationnameunit
-
-    def test_authenticate__check_user_extended_data(self, app, institution, url_auth_institution):
-        username, fullname, password = 'user_active@user.edu', 'Foo Bar', 'FuAsKeEr'
-        user = make_user(username, fullname)
-        user.set_password(password)
-        user.save()
-
-        with capture_signals() as mock_signals:
-            res = app.post(
-                url_auth_institution,
-                make_payload(
-                    institution,
-                    username,
-                    family_name='User',
-                    given_name='Fake',
-                    fullname='Fake User',
-                    department='Fake Department',
-                    login_availability='can login',
-                )
-            )
-        assert res.status_code == 204 or res.status_code == 200
-        assert not mock_signals.signals_sent()
-
-        # confirm login availability extended data
-        extended_data = UserExtendedData.objects.get(user=user)
-        assert extended_data.data.get('idp_attr', {}).get('login_availability') == 'can login'
 
     @mock.patch('api.institutions.authentication.login_by_eppn')
     def test_with_new_attribute(self, mock, app, institution, url_auth_institution):
