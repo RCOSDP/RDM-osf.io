77acdbca65d89596d880a63531d50768dc8aa08d
hide24 <hidetoshi.yoshimoto@gmail.com>
2026-02-04 13:37:00 +0900

    remove unused import

--- api_tests/institutions/views/test_institution_auth.py
+++ api_tests/institutions/views/test_institution_auth.py
@@ -12,7 +12,6 @@ from framework.auth import signals, Auth
 from framework.auth.views import send_confirm_email
 
 from osf.models import OSFUser
-from osf.models import UserExtendedData
 from osf_tests.factories import InstitutionFactory, ProjectFactory, UserFactory
 
 from tests.base import capture_signals
