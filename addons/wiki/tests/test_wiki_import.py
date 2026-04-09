be7f448bc53e227e8c177d06c211e5727bd7a4dc
Nishikawa Koharu <nishikawa@omik.co.jp>
2026-04-09 14:05:27 +0900

    TraviceCI

--- addons/wiki/tests/test_wiki_import.py
+++ addons/wiki/tests/test_wiki_import.py
@@ -124,7 +124,7 @@ class TestWikiPageNodeManager(OsfTestCase, unittest.TestCase):
 
         # True?
         mock_create.assert_called_with(is_wiki_import=True, node=self.project, page_name='home child', parent=None, user=self.user)
-        wiki_page.update.assert_called_with(self.user, 'home child content', is_wiki_import=True)
+        wiki_page.update.assert_called_with(self.user, 'home child content', add_log=True, is_wiki_import=True)
 
     @mock.patch('addons.wiki.models.WikiPage.objects.create')
     def test_create_for_node_false(self, mock_create):
@@ -142,7 +142,7 @@ class TestWikiPageNodeManager(OsfTestCase, unittest.TestCase):
 
         # False
         mock_create.assert_called_with(is_wiki_import=False, node=self.project, page_name='home child', parent=None, user=self.user)
-        wiki_page.update.assert_called_with(self.user, 'home child content', is_wiki_import=False)
+        wiki_page.update.assert_called_with(self.user, 'home child content', add_log=True, is_wiki_import=False)
 
     def test_create(self):
         new_node = WikiPage.objects.create(
