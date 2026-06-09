a39a071821a951053ac34060820b94e1a951d88b
hcphat <hcphat@tma.com.vn>
2026-06-09 19:08:00 +0700

    ref 2.1.小規模大量ファイルを含むフォルダアップロード性能改善にかかる開発: Rework review comment

--- website/static/js/fangorn.js
+++ website/static/js/fangorn.js
@@ -115,7 +115,14 @@ var PROVIDER_SETTINGS = {
     'figshare': { parallelNum: 1, fileSizeThreshold: null },
     'github': { parallelNum: 1, fileSizeThreshold: null },
     'swift': { parallelNum: 1, fileSizeThreshold: null },
-    'owncloud': { parallelNum: 1, fileSizeThreshold: null }
+    'owncloud': { parallelNum: 1, fileSizeThreshold: null },
+
+    // Institution storage settings
+    'dropboxbusiness': { parallelNum: 1, fileSizeThreshold: null },
+    'nextcloudinstitutions': { parallelNum: 4, fileSizeThreshold: 128000000 },
+    's3compatinstitutions': { parallelNum: 4, fileSizeThreshold: 128000000 },
+    'onedrivebusiness': { parallelNum: 4, fileSizeThreshold: 128000000 },
+    'ociinstitutions': { parallelNum: 4, fileSizeThreshold: 128000000 }
 };
 
 // Monkey-patch Dropzone.processQueue here (must run before any Dropzone instances are created)
@@ -1088,6 +1095,9 @@ function _fangornDropzoneDrop(treebeard, event) {
     if (providerSettings && providerSettings.parallelNum && providerSettings.fileSizeThreshold) {
         treebeard.dropzone.options.parallelUploads = providerSettings.parallelNum;
         treebeard.dropzone.options.fileSizeThreshold = providerSettings.fileSizeThreshold;
+    } else {
+        treebeard.dropzone.options.parallelUploads = 1;
+        treebeard.dropzone.options.fileSizeThreshold = null;
     }
 }
 /**
@@ -1343,6 +1353,7 @@ function _uploadEvent(event, item, col) {
         }else{
             // Default settings
             self.dropzone.options.parallelUploads = 1;
+            self.dropzone.options.fileSizeThreshold = null;
         }
 
         // Add files to Treebeard
@@ -1457,6 +1468,7 @@ function _uploadFolderEvent(event, item, mode, col) {
             tb.dropzone.options.fileSizeThreshold = providerSettings.fileSizeThreshold;
         }else{
             tb.dropzone.options.parallelUploads = 1;
+            tb.dropzone.options.fileSizeThreshold = null;
         }
 
         var createdFolders = [];
